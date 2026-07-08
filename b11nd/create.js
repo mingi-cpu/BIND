12993fe51504e16d471d61a43135f3ce52f43d8c
mingi-cpu <a01062474215@gmail.com>
2026-07-08 16:35:30 +0900

    feat:파일생성

---
+++ b11nd/create.js
@@ -0,0 +1,32 @@
+const userInput = document.querySelector("#userInput");
+const titleInput = document.querySelector("#titleInput");
+const postButton = document.querySelector("#post-button");
+async function onClickPostButton(){
+    const postData = {
+        title:titleInput.value,
+        content:userInput.value
+    }
+    console.log(postData)
+    try {
+        const response = await fetch(`http://localhost:8080/api/v1/posts`,{
+        method:"POST",
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        credentials:"include",
+        body:JSON.stringify(postData)
+    })
+    const result = await response.json();
+    console.log(result)
+    if(result.success === true){
+        alert(result.message)
+        window.location.href = 'main.html';
+    }else{
+        alert(result.message)
+    }}catch(error){
+        console.error("통신에러",error)
+    }
+}
+const userName = document.querySelector(".user p");
+userName.innerHTML=localStorage.getItem("isLogin")
+postButton.addEventListener("click",onClickPostButton);
